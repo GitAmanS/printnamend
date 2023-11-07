7104197e958d26ab6fe04fd9509c495b9ba03437
Aman Shaikh <amanshaikh8624@gmail.com>
2023-11-07 04:20:46 -0800

    routes

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
-function rqListener(req, res){
-    console.log("Aman Shaikh");
-}
+const server = http.createServer((req, res) => {
+    res.write(`<html>`);
+    res.write(`<head><title>node js</title><head>`);
 
-const Server = http.createServer(rqListener);
+    if (req.url === '/home') {
+        res.write(`<h1>Welcome home</h1>`);
+        res.end();
+    } else if (req.url === '/about') {
+        res.write(`<h1>Welcome to About Us page</h1>`);
+        res.end();
+    } else if (req.url === '/node') {
+        res.write(`<h1>Welcome to my Node Js project</h1>`);
+        res.end();
+    }
+    res.write(`</html>`);
+});
 
-Server.listen(3000);
+
+server.listen(4000);
