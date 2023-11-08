81c0f966f61455403c56a7b1b6dfcf7075857731
Aman Shaikh <amanshaikh8624@gmail.com>
2023-11-07 23:48:23 -0800

    exports

--- main.js
+++ main.js
@@ -1,49 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(`
-            <html>
-            <head>
-                <title>Node.js Server</title>
-            </head>
-            <body>
-                <form action="/" method="POST">
-                    <input type='text' name='inputText'>
-                    <button type="submit">Send</button>
-                </form>
-            </body>
-            </html>
-        `);
-        res.end();
-    } else if (req.method === 'POST' && req.url === '/') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const message = body.split('=')[1];
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(`
-                <html>
-                <head>
-                    <title>Node.js Server</title>
-                </head>
-                <body>
-                    <h2>${decodeURIComponent(message)}</h2>
-                    <form action="/" method="POST">
-                        <input type='text' name='inputText'>
-                        <button type="submit">Send</button>
-                    </form>
-                </body>
-                </html>
-            `);
-            res.end();
-        });
-    }
-});
+const routes = require('./routes');
 
-server.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+const server = http.createServer(routes);
+
+server.listen(4000);
---
+++ routes.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+const requestHandler = (req, res)=>{
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`
+            <html>
+            <head>
+                <title>Node.js Server</title>
+            </head>
+            <body>
+                <form action="/" method="POST">
+                    <input type='text' name='inputText'>
+                    <button type="submit">Send</button>
+                </form>
+            </body>
+            </html>
+        `);
+        res.end();
+    } else if (req.method === 'POST' && req.url === '/') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const message = body.split('=')[1];
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(`
+                <html>
+                <head>
+                    <title>Node.js Server</title>
+                </head>
+                <body>
+                    <h2>${decodeURIComponent(message)}</h2>
+                    <form action="/" method="POST">
+                        <input type='text' name='inputText'>
+                        <button type="submit">Send</button>
+                    </form>
+                </body>
+                </html>
+            `);
+            res.end();
+        });
+    }
+}
+
+module.exports= requestHandler;
